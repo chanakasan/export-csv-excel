b42aa307859c09aef1a3de75ae7ea64f0a76d2c7
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-08-31 10:00:18 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExportCsvExcel::Application.config.secret_token = 'ad8985f22055b6fd822716bc275ea3a1f10d2480ed83a08d4519bef11ca28fd87ca324024cdbc9797291c1f7a2a576803fdff60a238c4b208dfdc4156e816e7e'
